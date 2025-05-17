36febc5a98f1ce199dd052e2c13b65416c69b085
esenyel <14781522+esenyel@users.noreply.github.com>
2025-05-17 20:26:15 +0000

    Deploying to gh-pages from @ risl-rpi/risl-rpi.github.io@6b3aa48ae03b7aeadbbdb230c5fc48d33d845c59 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,9 +419,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-predictive-autonomy",
+          title: 'Predictive Autonomy',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
