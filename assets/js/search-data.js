e158671164fb11433ebae7d911aea571fe8f1ac6
esenyel <14781522+esenyel@users.noreply.github.com>
2025-05-18 01:47:46 +0000

    Deploying to gh-pages from @ risl-rpi/risl-rpi.github.io@72a17784ee2be0f030248af17b13a126f26c8a28 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "Members of the lab",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
